532062afdedaa3f7373bc250c587034066373e22
NeQuispe <nelsonmil15@outlook.com>
2023-11-16 20:18:47 -0300

    Se crea vista "About" y se modifican templates

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home/index.html"), name="index"),
+    path("about", TemplateView.as_view(template_name="home/about.html"), name="about"),
 ]
